cdcfa8e92e4c280f274e29d407c6a1b838338910
Vyctor <dev.vyctor@gmail.com>
2023-01-06 01:05:53 -0300

    Teste de abstracao de validacao parte 2

--- src/@seedwork/validators/validator-rules.spec.ts
+++ src/@seedwork/validators/validator-rules.spec.ts
@@ -1,5 +1,41 @@
 import { ValidatorRules } from "./validator-rules";
 import { ValidationError } from "../errors/validation-error";
+
+type ExpectedRule = {
+  value: any;
+  property: string;
+  rule: keyof ValidatorRules;
+  error: ValidationError;
+  params?: any[];
+};
+
+type ExpectedValidationRule = {
+  value: any;
+  property: string;
+};
+
+function assertIsInvalid({
+  value,
+  property,
+  rule,
+  error,
+  params,
+}: ExpectedRule): void {
+  expect(() => {
+    const validator = ValidatorRules.values(value, property);
+    const method = validator[rule] as (...args: any[]) => ValidatorRules;
+    method.apply(validator, params);
+  }).toThrow(error);
+}
+
+function assertIsValid({ value, property, rule, error, params }: ExpectedRule) {
+  expect(() => {
+    const validator = ValidatorRules.values(value, property);
+    const method = validator[rule] as (...args: any[]) => ValidatorRules;
+    method.apply(validator, params);
+  }).not.toThrow(error);
+}
+
 describe("ValidatorRules Unit Tests", () => {
   test("values method", () => {
     const validator = ValidatorRules.values("value", "property");
@@ -11,23 +47,57 @@ describe("ValidatorRules Unit Tests", () => {
   });
 
   test("required validation rule method", () => {
-    let validator = ValidatorRules.values("value", "property");
-    expect(() => validator.required()).not.toThrow();
+    let arrange: ExpectedValidationRule[] = [
+      {
+        value: null,
+        property: "field",
+      },
+      {
+        value: undefined,
+        property: "field",
+      },
+      {
+        value: "",
+        property: "field",
+      },
+    ];
 
-    validator = ValidatorRules.values("", "property");
-    expect(() => validator.required()).toThrow(ValidationError);
+    arrange.forEach((item) => {
+      assertIsInvalid({
+        value: item.value,
+        property: item.property,
+        rule: "required",
+        error: new ValidationError("Property field is required"),
+      });
+    });
 
-    validator = ValidatorRules.values(null, "property");
-    expect(() => validator.required()).toThrow(ValidationError);
+    arrange = [
+      {
+        value: "value",
+        property: "field",
+      },
+      {
+        value: 5,
+        property: "field",
+      },
+      {
+        value: false,
+        property: "field",
+      },
+      {
+        value: 0,
+        property: "field",
+      },
+    ];
 
-    validator = ValidatorRules.values(undefined, "property");
-    expect(() => validator.required()).toThrow(ValidationError);
-
-    validator = ValidatorRules.values(0, "property");
-    expect(() => validator.required()).not.toThrow();
-
-    validator = ValidatorRules.values(false, "property");
-    expect(() => validator.required()).not.toThrow();
+    arrange.forEach((item) => {
+      assertIsValid({
+        value: item.value,
+        property: item.property,
+        rule: "required",
+        error: new ValidationError("Property field is required"),
+      });
+    });
   });
 
   test("string validation rule method", () => {
